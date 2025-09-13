4b66bb915dac836aa2dc0d45365396571c09d740
experiencei <ayelojahighbee01@gmail.com>
2025-09-13 16:31:02 +0100

    change assets folder

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import grainImage from '@/assets/images/map.png';
+// import grainImage from '@/assets/images/map.png';
 import { ComponentPropsWithoutRef } from "react"; 
 import {twMerge} from "tailwind-merge";
 
@@ -6,10 +6,10 @@ export const Card = ({ className, children , ...other } : ComponentPropsWithoutR
   return (
     <div  className={twMerge("bg-gray-800 z-0 overflow-hidden after:z-10 rounded-3xl relative after:content-[''] after:absolute after:inset-0  after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none", className)} {...other}>
 
-        <div className="absolute inset-0  -z-10 opacity-10" style = {{
+        {/* <div className="absolute inset-0  -z-10 opacity-10" style = {{
           backgroundImage:
             `url(${grainImage.src})`,
-        }}></div>
+        }}></div> */}
         {children}
         </div>
   );
